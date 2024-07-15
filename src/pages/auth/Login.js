5d1a51847a743e96c3148ae959e3d9dff1921294
Shiva <seebaw.m@gmail.com>
2024-07-15 23:34:15 +1000

    admin signup and public signup

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import DefaultLayout from '../../components/layouts/DefaultLayout';
 import { Button, Form } from 'react-bootstrap';
 import CustomInput from '../../components/customInput/CustomInput';
+import { toast } from 'react-toastify';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../config/firebase-config';
 
 const inputs = [
     {
@@ -31,7 +34,23 @@ function Login() {
 
     };
 
-    const handleOnSubmit = () => {};
+    const handleOnSubmit = async (e) => {
+      e.preventDefault();
+      try{const { email, password } = formData;
+      const signInPromise = signInWithEmailAndPassword(auth, email, password);
+      toast.promise(signInPromise, {
+        pending: "In Progress...",
+      });
+      const { user } = await signInPromise;
+      toast.success("Login in Successful");
+      // pull the user info from DB
+
+      }
+      catch{
+        toast.error(`Something went wrong ${e.message}`);
+
+      }
+    };
 
 
   return (
